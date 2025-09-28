2737188d7a403df295f9c1564a58e1fed7851672
Muhammad Hassan <mehassan3454@gmail.com>
2025-09-28 08:56:05 +0500

    User Role

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -29,6 +29,11 @@ const userSchema = new mongoose.Schema(
       },
     ],
     refreshToken: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
   { timestamps: true }
 );
