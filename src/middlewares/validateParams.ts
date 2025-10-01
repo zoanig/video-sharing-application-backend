311e5ca99c202898014ea19b6697bab1a848cd11
Muhammad Hassan <mehassan3454@gmail.com>
2025-10-02 00:41:10 +0500

    Params Validator (lazy ik)

---
+++ src/middlewares/validateParams.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodError, ZodObject } from "zod";
+import { ApiError } from "../utils/ApiError";
+
+export default (schema: ZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.params);
+      next();
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const code = 400;
+        return res
+          .status(code)
+          .json(
+            new ApiError(code, "Validation Failed", JSON.parse(err.message))
+          );
+      } else {
+        const code = 500;
+        return res.status(code).json(new ApiError(code));
+      }
+    }
+  };
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
+import { z } from "zod";
 import { userLoginSchema } from "./validators/userLogin";
 import { userSignUpSchema } from "./validators/userSignUp";
-import { z } from "zod";
 import { videoUploadSchema } from "./validators/videoUpload";
+import {
+  videoUpdateParamsSchema,
+  videoUpdateSchema,
+} from "./validators/videoUpdate";
 
 export type userSignUpType = z.infer<typeof userSignUpSchema>;
 export type userLoginType = z.infer<typeof userLoginSchema>;
 export type videoUploadType = z.infer<typeof videoUploadSchema>;
+export type videoUpdateType = z.infer<typeof videoUpdateSchema>;
+export type videoUpdateParamsType = z.infer<typeof videoUpdateParamsSchema>;
 
 export type userRole = "user" | "admin";
 export interface payload {
