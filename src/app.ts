3d8030c23e1ead7de8daa3e4a7761d5dcfc604b7
Muhammad Hassan <mehassan3454@gmail.com>
2025-10-06 10:40:37 +0500

    Use routes in app

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ const app = express();
 
 import userRouter from "./routes/user.routes";
 import videoRouter from "./routes/video.routes";
+import likeRouter from "./routes/like.routes";
+import playlistRouter from "./routes/playlist.routes";
+import commentRouter from "./routes/comment.routes";
+import subcriptionRouter from "./routes/subscription.routes";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,5 +15,9 @@ app.use(express.static("public"));
 
 app.use("/api/user", userRouter);
 app.use("/api/video", videoRouter);
+app.use("/api/like", likeRouter);
+app.use("/api/playlist", playlistRouter);
+app.use("/api/comment", commentRouter);
+app.use("/api/subscription", subcriptionRouter);
 
 export { app };
